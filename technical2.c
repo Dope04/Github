146eea3f70f311b159353b2e0bbc721edd434fed
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2023-11-01 14:27:41 +0530

    modified

--- technical2.c
+++ technical2.c
@@ -5,11 +5,11 @@ int main() {
     double num1, num2, result;
     char operator;
 
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
-
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
 
     printf("Enter second number: ");
     scanf("%lf", &num2);
