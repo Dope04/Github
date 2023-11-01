d80643fe0f30e2ce8fce8a2a2eca0dd37f5d720c
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2023-11-01 14:15:11 +0530

    modified

--- technical.c
+++ technical.c
@@ -1,15 +1,14 @@
-//
-// Created by monda on 01-11-2023.
-//
+// WAP in c to print multiplaction table
+
 #include <stdio.h>
 
 int main() {
     int num;
-    printf("enter a number:");
+    printf("Enter a number:");
     scanf("%d",&num);
     printf("Multiplication Table for %d:\n", num);
     for (int i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", num, i, num * i);
+        printf("%d x %d = %d\n",num, i, num * i);
     }
 
     return 0;
